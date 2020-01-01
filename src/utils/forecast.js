2e648518c15e4c141a3ed088557a540180f72f56
unknown <shomendrap@gmail.com>
2020-01-02 04:22:50 +0530

    add high and low forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,6 @@
 
 
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+
 
 const request = require('request')
 
@@ -21,7 +14,8 @@ const forecast = (latitude, longitude, callback) =>{
        }else if (body.error) {
         callback('unable to connect to location !', undefined)
        }else{
-        callback(undefined, body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+         
+        callback(undefined, body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' degrees out. This high today is' + body.daily.data[0].temperatureHigh + 'with a low of' + body.daily.data[0].temperatureLow + '.  There is a ' + body.currently.precipProbability + '% chance of rain')
        }
 
      })
